ace387b01d858cbbd27229b9fb5d5df028cbd024
unknown <jeancarlosrafaelrosales@gmail.com>
2020-09-14 16:53:42 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'vdst-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'bpm-mf-pre-filter';
+}
