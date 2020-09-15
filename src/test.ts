3fc3b6ad469d94d2470133e150a11897abb38122
unknown <jeancarlosrafaelrosales@gmail.com>
2020-09-15 09:53:37 -0500

    fix(tsconfig.ts): clean comment

--- src/test.ts
+++ src/test.ts
@@ -1,29 +1,29 @@
-import 'zone.js/dist/zone-testing';
-import { getTestBed } from '@angular/core/testing';
-import {
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting,
-} from '@angular/platform-browser-dynamic/testing';
+// import 'zone.js/dist/zone-testing';
+// import { getTestBed } from '@angular/core/testing';
+// import {
+//   BrowserDynamicTestingModule,
+//   platformBrowserDynamicTesting,
+// } from '@angular/platform-browser-dynamic/testing';
 
 import 'jest-preset-angular';
 
-declare const require: {
-  context(
-    path: string,
-    deep?: boolean,
-    filter?: RegExp,
-  ): {
-    keys(): string[];
-    <T>(id: string): T;
-  };
-};
+// declare const require: {
+//   context(
+//     path: string,
+//     deep?: boolean,
+//     filter?: RegExp,
+//   ): {
+//     keys(): string[];
+//     <T>(id: string): T;
+//   };
+// };
 
-// First, initialize the Angular testing environment.
-getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
-// Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
-// And load the modules.
-context.keys().map(context);
+// // First, initialize the Angular testing environment.
+// getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
+// // Then we find all the tests.
+// const context = require.context('./', true, /\.spec\.ts$/);
+// // And load the modules.
+// context.keys().map(context);
 
 Object.defineProperty(window, 'CSS', { value: null });
 Object.defineProperty(window, 'getComputedStyle', {
